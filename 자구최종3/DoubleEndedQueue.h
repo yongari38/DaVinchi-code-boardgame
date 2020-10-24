7774dd945efcf8e6972b2ac9f0423cd0c183b815
yongari38 <yongari38@naver.com>
2020-10-24 18:54:29 +0900

    remove dir

--- 자구최종3/DoubleEndedQueue.h
+++
@@ -1,9 +0,0 @@
-void dq_init(Deque * pd);
-int dq_isEmpty(Deque * pd);
-int dq_isFrontEmpty(Deque * pd);
-int dq_isRearEmpty(Deque * pd);
-void dq_addFront(Deque * pd, Block * newBlock);
-void dq_addRear(Deque * pd, Block * newBlock);
-Block * dq_deleteFront(Deque * pd);
-Block * dq_deleteRear(Deque * pd);
-void dq_display(Deque * pd);
